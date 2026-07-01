fad904ac49eab7320c11f06d0ebdf1ee182e9488
kristina_kamagorova <kamagorovakristina@gmail.com>
2026-07-01 23:33:54 +0700

    Create base components

---
+++ src/app/3-section/section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  imports: [],
+  templateUrl: './section.html',
+  styleUrl: './section.scss',
+})
+export class Section {}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Nav} from './1-nav/nav';
+import { Hero } from './2-hero/hero';
+import { Section } from './3-section/section';
+import { Blog } from './4-blog/blog';
+import { Footer } from './5-footer/footer';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [Nav, Hero, Section, Blog, Footer],
   styleUrl: './app.scss'
 })
 export class App {
